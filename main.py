9c9fa18421fa97ea65313cf033adf1f6b4d23353
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-04-03 01:04:09 +0200

    added category and date

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from muzikantenbankeu import MuzikantenBankEU
 from dict_to_text import DictToText
+import datetime
 muzikantenbank_eu = MuzikantenBankEU('musician', instrument='bassist',
                                      province='gelderland', )
-
+today = datetime.datetime.now()
 
 with open('muzikantenbank_eu_results.txt', 'a') as muzbanktxt:
+    muzbanktxt.write(f'Muzikantenbank.eu results of {today}\n')
     for thing in muzikantenbank_eu.results:
         dict_to_text = DictToText(thing)
         muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
