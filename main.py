ea6bcc9c98caba19526c6e27493bb898c84a0c3e
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-04-02 19:07:33 +0200

    changed few small things and added dict_text_py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from muzikantenbankeu import MuzikantenBankEU
+from dict_to_text import DictToText
+muzikantenbank_eu = MuzikantenBankEU('musician', instrument='bassist',
+                                     province='gelderland', )
 
-test = MuzikantenBankEU('musician', instrument='bassist',
-                        province='gelderland', )
+
+with open('muzikantenbank_eu_results.txt', 'a') as muzbanktxt:
+    for thing in muzikantenbank_eu.results:
+        dict_to_text = DictToText(thing)
+        muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
