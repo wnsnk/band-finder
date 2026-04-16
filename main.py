788d0bb2505f09f912055e39cb37f69388322bbb
cas <cas.wensink@hotmail.nl>
2026-04-16 18:06:05 +0200

    added support for puppunt gelderland prikbord

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from muzikantenbankeu import MuzikantenBankEU
 from muzikantenbanknet import MuzikantenBankNet
+from poppuntgelderland import PopPuntGelderlandPrikbord
 from dict_to_text import DictToText
 import datetime
 muzikantenbank_eu = MuzikantenBankEU('musician', instrument='bassist',
                                      province='gelderland', )
 muzikantenbank_net = MuzikantenBankNet('bassist gelderland')
+poppunt_gelderland = PopPuntGelderlandPrikbord()
 today = datetime.datetime.now()
 
 with open('muzikantenbank_eu_results.txt', 'a') as muzbanktxt:
     muzbanktxt.write(f'Muzikantenbank.eu results of {today}\n')
-    for thing in muzikantenbank_eu.results:
-        dict_to_text = DictToText(thing)
+    for result in muzikantenbank_eu.results:
+        dict_to_text = DictToText(result)
         muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
 
 with open('muzikantenbank_net_results.txt', 'a') as muzbanktxt:
     muzbanktxt.write(f'Muzikantenbank.net results of {today}\n')
-    for thing in muzikantenbank_net.results:
-        dict_to_text = DictToText(thing)
+    for result in muzikantenbank_net.results:
+        dict_to_text = DictToText(result)
         muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
 
-# branch is now merged
+with open('poppuntgelderland_resulst.txt', 'a') as poppunttxt:
+    poppunttxt.write(f'Poppuntgelderland results of {today}\n')
+    for result in poppunt_gelderland.results:
+        dict_to_text = DictToText(result)
+        poppunttxt.write(f'{dict_to_text.return_advertisement()}\n')
---
+++ poppuntgelderland.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class PopPuntGelderlandPrikbord():
+
+    def __init__(self):
+        self.url = 'https://poppuntgelderland.nl/prikbord/'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:149.0) Gecko/20100101 Firefox/149.0'}
+        self.results = self.search_website()
+
+    def search_website(self) -> list:
+        '''Returns a list of dictionaries with info all advertisements'''
+
+        self.response = requests.get(url=self.url, headers=self.headers)
+        self.response.raise_for_status()
+        self.html = self.response.text
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        self.all_advertisements = []
+        self.advertisements = self.soup.find_all(class_='prikbordBlok')
+        for ad in self.advertisements:
+            self.title = ad.find('h3', class_='big').text
+            self.category = ad.find('h4', class_='bold').text
+            self.date = ad.find_all('h4')[1].text
+            self.message = ''
+            self.link = ad.find('a', href=True)['href']
+
+            self.info = {
+                'title': self.title,
+                'category': self.category,
+                'message': self.message,
+                'link': self.link,
+                'date': self.date
+            }
+            self.all_advertisements.append(self.info)
+        return self.all_advertisements
