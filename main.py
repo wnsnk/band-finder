4b976d74c0c89491e4bca6fbcf0b1bfdb6ecf4ae
cas <cas.wensink@hotmail.nl>
2026-04-16 19:43:05 +0200

    First working version seeing results in html

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 from muzikantenbankeu import MuzikantenBankEU
 from muzikantenbanknet import MuzikantenBankNet
 from poppuntgelderland import PopPuntGelderlandPrikbord
@@ -9,20 +10,35 @@ muzikantenbank_net = MuzikantenBankNet('bassist gelderland')
 poppunt_gelderland = PopPuntGelderlandPrikbord()
 today = datetime.datetime.now()
 
-with open('muzikantenbank_eu_results.txt', 'a') as muzbanktxt:
-    muzbanktxt.write(f'Muzikantenbank.eu results of {today}\n')
-    for result in muzikantenbank_eu.results:
-        dict_to_text = DictToText(result)
-        muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
+all_results = muzikantenbank_eu.results + \
+    muzikantenbank_net.results + poppunt_gelderland.results
 
-with open('muzikantenbank_net_results.txt', 'a') as muzbanktxt:
-    muzbanktxt.write(f'Muzikantenbank.net results of {today}\n')
-    for result in muzikantenbank_net.results:
-        dict_to_text = DictToText(result)
-        muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
+app = Flask(__name__)
 
-with open('poppuntgelderland_resulst.txt', 'a') as poppunttxt:
-    poppunttxt.write(f'Poppuntgelderland results of {today}\n')
-    for result in poppunt_gelderland.results:
-        dict_to_text = DictToText(result)
-        poppunttxt.write(f'{dict_to_text.return_advertisement()}\n')
+
+@app.route("/")
+def show_results():
+    return render_template('results.html', articles=all_results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# with open('muzikantenbank_eu_results.txt', 'a') as muzbanktxt:
+#     muzbanktxt.write(f'Muzikantenbank.eu results of {today}\n')
+#     for result in muzikantenbank_eu.results:
+#         dict_to_text = DictToText(result)
+#         muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
+
+# with open('muzikantenbank_net_results.txt', 'a') as muzbanktxt:
+#     muzbanktxt.write(f'Muzikantenbank.net results of {today}\n')
+#     for result in muzikantenbank_net.results:
+#         dict_to_text = DictToText(result)
+#         muzbanktxt.write(f'{dict_to_text.return_advertisement()}\n')
+
+# with open('poppuntgelderland_resulst.txt', 'a') as poppunttxt:
+#     poppunttxt.write(f'Poppuntgelderland results of {today}\n')
+#     for result in poppunt_gelderland.results:
+#         dict_to_text = DictToText(result)
+#         poppunttxt.write(f'{dict_to_text.return_advertisement()}\n')
