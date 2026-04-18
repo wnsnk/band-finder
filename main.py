4d389aaaad8ece2cdf531837ad73297e8658b64d
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-04-18 23:42:58 +0200

    made a user interface in the homepage

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField, SelectMultipleField, BooleanField
 from wtforms.validators import DataRequired, Email, Length
 from flask_bootstrap import Bootstrap5
-from webscrapers.muzikantenbankeu import MuzikantenBankEU
+from webscrapers.muzikantenbankeu import MuzikantenBankEU, instrument_options, province_options_netherlands
 from webscrapers.muzikantenbanknet import MuzikantenBankNet
 from webscrapers.poppuntgelderland import PopPuntGelderlandPrikbord
 from dict_to_text import DictToText
 import datetime
 
-# webscrapers:
-muzikantenbank_eu = MuzikantenBankEU('musician', instrument='bassist',
-                                     province='gelderland', )
-muzikantenbank_net = MuzikantenBankNet('bassist gelderland')
-poppunt_gelderland = PopPuntGelderlandPrikbord()
-today = datetime.datetime.now()
 
-all_results = muzikantenbank_eu.results + \
-    muzikantenbank_net.results + poppunt_gelderland.results
-with open('results.txt', 'w') as resultstxt:
-    resultstxt.write(str(all_results))
+today = datetime.datetime.now()
 
 
 # flask:
@@ -29,23 +20,59 @@ Bootstrap = Bootstrap5(app=app)
 
 
 class SearchForm(FlaskForm):
+
     looking_for = RadioField(label='Ik zoek een:', choices=[
-                             'band', 'muzikant'], validators=[DataRequired()])
-    looking_for_specifically = SelectField(
-        label='Wat voor muzikant? ', choices=['guitar', 'bass', 'drums'])
-    country = RadioField(label='Land', choices=['Nederland', 'België'])
-    province = SelectField(label='Provincie')
+                             'band', 'muzikant'])
+    instrument = SelectField(
+        label='Wat voor muzikant? ', choices=instrument_options)
+    # country = RadioField(label='Land', choices=['Nederland', 'België'])
+    province = SelectField(
+        label='Provincie', choices=province_options_netherlands)
+    muzbankeu = BooleanField(
+        label='muzikantenbank.eu', )
+    muzbanknet = BooleanField(
+        label='muzikantenbank.net', )
+    poppunt = BooleanField(label='Poppunt Gelderland')
+    submit = SubmitField('Zoeken.')
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home_page():
     search_form = SearchForm()
-    return render_template('index.html', search_form=search_form)
+
+    if search_form.validate_on_submit():
+        looking_for = search_form.looking_for.data
+        instrument = search_form.instrument.data
+        # country = search_form.country
+        province = search_form.province.data
+
+# TODO catch valueerror and unbounderror and other bugs
+        all_results = []
+        if search_form.muzbankeu.data:
+            muzikantenbank_eu = MuzikantenBankEU(looking_for=looking_for, instrument=instrument,
+                                                 province=province)
+            for result in muzikantenbank_eu.results:
+                all_results.append(result)
+        if search_form.muzbanknet.data:
+            muzikantenbank_net = MuzikantenBankNet(
+                f'{instrument} {province}')
+            for result in muzikantenbank_net.results:
+                all_results.append(result)
+
+        if search_form.poppunt.data:
+            poppunt_gelderland = PopPuntGelderlandPrikbord()
+            for result in poppunt_gelderland.results:
+                all_results.append(result)
+
+        return render_template('results.html', articles=all_results)
+    else:
+
+        return render_template('index.html', search_form=search_form)
 
 
-# @app.route('/results')
-# def show_results():
-#     return render_template('results.html', articles=all_results)
+@app.route('/results')
+def show_results():
+    return render_template('results.html')
 
 
 if __name__ == '__main__':
