839ecde1d960f649a9768674e2e6a672dd292772
cas <cas.wensink@hotmail.nl>
2026-04-18 22:13:42 +0200

    added picture for homepage

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, RadioField, SelectField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 from webscrapers.muzikantenbankeu import MuzikantenBankEU
 from webscrapers.muzikantenbanknet import MuzikantenBankNet
 from webscrapers.poppuntgelderland import PopPuntGelderlandPrikbord
 from dict_to_text import DictToText
 import datetime
+
+# webscrapers:
 muzikantenbank_eu = MuzikantenBankEU('musician', instrument='bassist',
                                      province='gelderland', )
 muzikantenbank_net = MuzikantenBankNet('bassist gelderland')
@@ -12,13 +18,34 @@ today = datetime.datetime.now()
 
 all_results = muzikantenbank_eu.results + \
     muzikantenbank_net.results + poppunt_gelderland.results
+with open('results.txt', 'w') as resultstxt:
+    resultstxt.write(str(all_results))
 
+
+# flask:
 app = Flask(__name__)
+app.secret_key = 'idk'
+Bootstrap = Bootstrap5(app=app)
 
 
-@app.route("/")
-def show_results():
-    return render_template('results.html', articles=all_results)
+class SearchForm(FlaskForm):
+    looking_for = RadioField(label='Ik zoek een:', choices=[
+                             'band', 'muzikant'], validators=[DataRequired()])
+    looking_for_specifically = SelectField(
+        label='Wat voor muzikant? ', choices=['guitar', 'bass', 'drums'])
+    country = RadioField(label='Land', choices=['Nederland', 'België'])
+    province = SelectField(label='Provincie')
+
+
+@app.route('/')
+def home_page():
+    search_form = SearchForm()
+    return render_template('index.html', search_form=search_form)
+
+
+# @app.route('/results')
+# def show_results():
+#     return render_template('results.html', articles=all_results)
 
 
 if __name__ == '__main__':
