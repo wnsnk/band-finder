36e980d5d9470904a70ba063556ab493bf826730
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-08-02 00:58:24 +0200

    fix wrong label text bug

--- static/script.js
+++ static/script.js
@@ -4,6 +4,12 @@ const radioButtonBand = document.getElementById('looking_for-0');
 const radioButtonMusician = document.getElementById('looking_for-1');
 const formLabels = document.getElementsByClassName('form-label');
 
+// Fixes wrong labeltext if page is reloaded.
+if (radioButtonBand.checked) {
+    formLabels[1].innerHTML = 'Ik ben een:';
+} else if (radioButtonMusician.checked) {
+    formLabels[1].innerHTML = 'Ik zoek een:'
+}
 
 
 radioButtonBand.addEventListener('change', function () {
